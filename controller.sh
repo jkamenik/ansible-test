c9c5cbacc5f124fe48f9190ac2179be6aeb9cebd
John Kamenik <jkamenik@gmail.com>
2015-10-28 23:03:01 -0400

    Adding ansible dependencies.

--- controller.sh
+++ controller.sh
@@ -10,7 +10,10 @@ sudo bash -c 'echo LC_ALL="en_US.UTF-8" >> /etc/default/locale'
 sudo apt-get -y autoremove
 sudo apt-get -q update
 sudo apt-get -y upgrade
-sudo apt-get -y install build-essential wget curl git-core ntp
+sudo apt-get -y install build-essential wget curl git-core ntp \
+     python-pip
+
+sudo pip install paramiko PyYAML Jinja2 httplib2 six
 
 # do cool stuff here
 if [ ! -d /vagrant/ansible ]; then
@@ -18,6 +21,8 @@ if [ ! -d /vagrant/ansible ]; then
   (cd /vagrant &&
    git clone http://github.com/ansible/ansible.git --recursive)
 fi
+
+sudo chown -R vagrant:vagrant /home/vagrant
 
 # cleanup
 sudo apt-get -y autoremove
