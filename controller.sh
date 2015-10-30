c8135c9e71af1a1014c75611f22a487de5b96dfd
John Kamenik <jkamenik@gmail.com>
2015-10-29 20:59:37 -0400

    Configuring the ansible controller

--- controller.sh
+++ controller.sh
@@ -2,6 +2,9 @@
 
 PATH=/usr/bin:/bin:/usr/sbin:/sbin
 DEBIAN_FRONTEND=noninteractive
+BASE_PATH=/vagrant/controller
+ANSIBLE_PATH=$BASE_PATH/ansible
+DEST_PATH=/home/vagrant
 
 set -x
 
@@ -11,17 +14,47 @@ sudo apt-get -y autoremove
 sudo apt-get -q update
 sudo apt-get -y upgrade
 sudo apt-get -y install build-essential wget curl git-core ntp \
-     python-pip
+     sshpass python-pip
 
 sudo pip install paramiko PyYAML Jinja2 httplib2 six
 
-# do cool stuff here
-if [ ! -d /vagrant/ansible ]; then
+# install ansible if it doesn't exit
+if [ ! -d $ANSIBLE_PATH ]; then
   echo "Cloning ansible"
-  (cd /vagrant &&
+  (cd $BASE_PATH &&
    git clone http://github.com/ansible/ansible.git --recursive)
 fi
 
+rm $DEST_PATH/ansible
+ln -s $ANSIBLE_PATH $DEST_PATH/ansible
+
+# move files
+function move(){
+  local file=$BASE_PATH/$1
+  local dest=$DEST_PATH/$2
+
+  echo "moving $file to $dest"
+  cp $file $dest
+}
+function link(){
+  local file=$BASE_PATH/$1
+  local dest=$DEST_PATH
+
+  if [ -n "$2" ]; then
+    dest="$dest/$2/$1"
+  else
+    dest="$dest/$1"
+  fi
+
+  echo "linking $file to $dest"
+  rm $dest
+  ln -s $file $dest
+}
+move .bashrc
+link .bash_ansible
+link ansible_hosts
+
+# fix permissions
 sudo chown -R vagrant:vagrant /home/vagrant
 
 # cleanup
